1ce9015d670d5283370d5cfa272bc65efc769b12
NSMY <95662519+NSMY@users.noreply.github.com>
2022-11-18 23:13:40 +1000

    breaks while Changes on Loop
    changed to search both \c and (86) folders for streamlink

--- Main.py
+++ Main.py
@@ -3,53 +3,68 @@ import sys
 import time
 from pyperclip import copy, paste
 import webbrowser
+from tkinter import *
+from tkinter import filedialog
 
+#Retrieves Last item in Clipboard(ctrl v)
 #clipboard.copy("goosfrabe")  # now the clipboard content will be string "abc"
 clp_brd = paste()  # text will have the content of clipboard
 url_ = clp_brd.replace("?filter=archives&sort=time","")
 
-# def chk_sLink():
-#     isthere = os.path.exists(r"C:\Program Files\Streamlink\bin\streamlink.exe")
-#     if isthere is False:
-#         print("\nYou do not seem to have streamLink installed.\nPlease Visit:\nhttps://github.com/streamlink/windows-builds/releases/latest\nto download Streamlink and install it\n")
-
-# chk_sLink()
-
-
+#Runs Check if streamlink is installed and gives link/opens if Not
 acptLst = ["yes", "y"]
 check_in = 0
 isthere = False
+isthere_86 = False
 while True:
-    isthere = os.path.exists(r"C:\Program Files\Streamlink\bin\streamlinkAA.exe")
-    if isthere is False and check_in == 0:
-        print("\nYou do not seem to have streamLink installed.\nPlease Visit:\nhttps://github.com/streamlink/windows-builds/releases/latest\nTo download Streamlink and install it.\n")
-        ipt_asw = input("Auto Launch Website?\ny/n?: ").lower()
-        if ipt_asw in acptLst:
-            webbrowser.open("https://github.com/streamlink/windows-builds/releases/latest")
-            check_in = +1
+    isthere = os.path.exists(r"C:\Program Files\Streamlink\bin\streamlink.exe")
+    time.sleep(1)
+    isthere_86 = os.path.exists(r"C:\Program Files (x86)\Streamlink\bin\streamlink.exe")
+    if check_in == 0 and isthere is False:
+        if isthere_86 is False:
+            print(f"\nYou do not seem to have streamLink installed.\nPlease Visit:\nhttps://github.com/streamlink/windows-builds/releases/latest\nTo download Streamlink and install it.\n\n----Look For EG: streamlink-5.1.0-1-py310-x86_64.exe----\n")
+            ipt_asw = input("Auto Launch Website?\ny/n?: ").lower()
+            if ipt_asw in acptLst:
+                webbrowser.open("https://github.com/streamlink/windows-builds/releases/latest")
+                check_in = +1
+            else:            
+                check_in = +1
+                continue
         else:
-            check_in = +1
-            continue
-    elif check_in >= 0 and isthere == False:
-        print("\nWaiting for Installation...")
-        time.sleep(7)
-        got_it = input("\nHas Streamlink been installed yet?\ny/n?:").lower()
-        if got_it in acptLst and isthere is False:
-            got_it = input("\nHaven't Found Stream Link In File Path\nC.Program.Files.Streamlink.bin      Are you sure? y/n?:").lower()
-        elif got_it not in acptLst:
-            print("\nWaiting!......")
-            time.sleep(20)
+            break
+    elif check_in >= 0 and isthere is False:
+        if isthere_86 is False:
+            print("\nWaiting for Installation...")
+            time.sleep(7)
+            got_it = input("\nHas Streamlink been installed yet?\ny/n?:").lower()
+            if got_it in acptLst and isthere is False or isthere_86 is False:
+                got_it = input("\nHaven't Found Stream Link In File Path\nC.Program.Files.Streamlink.bin      Are you sure? y/n?:").lower()
+            elif got_it not in acptLst:
+                print("\nWaiting!......")
+                time.sleep(20)
     else:
         break
 
+
+#opens Windows Save Folder and stores chosen path
+def saveFile():
+    file = filedialog.asksaveasfilename(defaultextension='.mp4',
+                                        filetypes=[
+                                            ("MP4 files",".mp4"),    
+                                            ("MOV files",".mov"),    
+                                            ("All files",".*"),
+                                        ])
+    return (file)
+file_path = saveFile()
+
+if len(file_path) ==0: #closes Program if No Save path is Entered
+    sys.exit ()
+
 sizes = ["worst", "160p", "360p", "480p", "720p", "720p60", "1080p", "1080p60","160p30", "360p30", "480p30", "720p30", "720p30", "1080p30", "source", "best"]
-name_of_File = input("Enter File Name:\n")
 
-
-file_path = fr'"E:\DeleteStreams\New folder\{name_of_File}.mp4"'
-
-
-
+#old Code
+# name_of_File = input("Enter File Name:\n")
+# file_path = fr'"E:\DeleteStreams\New folder\{name_of_File}.mp4"'
 
 def check_size(Vid_size):
     # print(Vid_size in sizes) Checks if True/False
@@ -59,9 +74,6 @@ def check_size(Vid_size):
         return ("Enter a Valid Size")     
 Fake_ = ""
 while True:
-    # if Fake_ == "":
-    #     os.system(fr'cmd /k "cd C:\Program Files\Streamlink\bin && streamlink "https://youtu.be/d9iR5CYa2io"')
-    #     time.sleep(3)
     if Fake_ == "":  
         Size_string = input("Please enter Resolution you want to download:\nworst, 160p, 360p, 480p, 720p, 720p60, 1080p60, source, best:\n").lower()  #stores Input into size_string Variable
         cmd_Str = check_size(Size_string)       #Runs Check_Size with Size_string {inputDATA} and Returns(stores) check-size if/else into cmd_Str Var
