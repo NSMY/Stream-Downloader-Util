67d3dffbaff396f82a0a8adb56265b709c6a709e
Nick <95662519+NSMY@users.noreply.github.com>
2022-11-21 19:04:03 +1000

    Updated Check t/be more modular

--- Main.py
+++ Main.py
@@ -3,7 +3,6 @@ import sys
 import time
 from pyperclip import copy, paste
 import webbrowser
-from tkinter import *
 from tkinter import filedialog
 
 #Retrieves Last item in Clipboard(ctrl v)
@@ -12,45 +11,65 @@ clp_brd = paste()  # text will have the content of clipboard
 url_ = clp_brd.replace("?filter=archives&sort=time","")
 
 #Runs Check if streamlink is installed and gives link/opens if Not
-acptLst = ["yes", "y"]
-check_in = 0
-isthere = False
-isthere_86 = False
-while True:
-    isthere = os.path.isfile(r"C:\Program Files\Streamlink\bin\streamlink.exe")
-    isthere_86 = os.path.exists(r"C:\Program Files (x86)\Streamlink\bin\streamlink.exe")
-    if check_in == 0 and isthere is False:
-        if isthere_86 is False:
-            print(f"\nYou do not seem to have streamLink installed.\nPlease Visit:\nhttps://github.com/streamlink/windows-builds/releases/latest\nTo download Streamlink and install it.\n\n----Look For EG: streamlink-5.1.0-1-py310-x86_64.exe----\n")
-            ipt_asw = input("Auto Launch Website?\ny/n?: ").lower()
-            if ipt_asw in acptLst:
-                webbrowser.open("https://github.com/streamlink/windows-builds/releases/latest")
-                check_in = +1
-            else:            
-                check_in = +1
-                continue
-        else:
-            break
-    elif check_in >= 0 and isthere is False:
-        if isthere_86 is False:
+accp_lst = {"yes": ["y", "yes"], "no": ["n", "no"]}
+
+def chk_if_Install():
+    stlink = []
+    stlink.append(os.path.isfile("C:\\Program Files\Streamlink\\bin\\streamlink.exe"))
+    stlink.append(os.path.isfile("C:\\Program Files (x86)\\Streamlink\\bin\\streamlink.exe"))
+    return stlink
+
+
+def setLink_Path():
+    if (chk_if_Install()[0]) == True:
+        lnk_pth = "C:\\Program Files\\Streamlink\\bin\\"
+        return lnk_pth
+    elif (chk_if_Install()[1]) == True:
+        lnk_pth = "C:\\Program Files (x86)\\Streamlink\\bin\\"
+        return lnk_pth            
+    else:
+        return ("404 Not Here")
+
+stream_lnk_Path = ""
+swtch = 1
+
+while stream_lnk_Path != "c":
+    stream_lnk_Path = setLink_Path()
+    if stream_lnk_Path == "404 Not Here" and swtch == 1:
+        swtch = +2
+        ipt_asw = input(f"\nYou do not seem to have streamLink installed.\n\nPlease "
+                "Visit: https://github.com/streamlink/windows-builds/releases/latest\nTo download "
+                "Streamlink and install it.\n\n----Look For EG: streamlink-5.1.0-1-py310-x86_64.exe----\nOR"
+                "\nAuto Launch Website? y/n?: ").lower()
+        if ipt_asw in accp_lst["yes"]:
+            webbrowser.open("https://github.com/streamlink/windows-builds/releases/latest")
             print("\nWaiting for Installation...")
-            time.sleep(7)
-            got_it = input("\nHas Streamlink been installed yet?\ny/n?:").lower()
-            if got_it in acptLst and isthere is False or isthere_86 is False:
-                lie_chk = input("\nHaven't Found Stream Link In File Path\nC.Program.Files.Streamlink.bin      Are you sure? y/n?:").lower()
-            elif lie_chk not in acptLst:
-                print("\nWaiting!......")
-                time.sleep(20)
+            time.sleep(15)
         else:
-            break
+            print("\nWaiting for Installation...")
+            time.sleep(7)               # TEMPORARY CODE ----15
+    elif swtch >= 0 and stream_lnk_Path == "404 Not Here":
+            swtch = +2
+            got_it = input("\nHas Streamlink been installed?\ny/n?:").lower()
+            stream_lnk_Path = setLink_Path()
+            time.sleep(5)
+            if got_it in accp_lst["yes"] and stream_lnk_Path == "404 Not Here":
+                stream_lnk_Path = setLink_Path()
+                lie_chk = input("\nHaven't Found Streamlink In File Path C\\Program (or {x86}) \\Files\\Streamlink\\bin\\"
+                                "   Are you sure it is there?\nDo you need the website again? y/n?:").lower()
+                time.sleep(5)
+                if lie_chk in accp_lst["yes"]:
+                    webbrowser.open("https://github.com/streamlink/windows-builds/releases/latest")
+                    print("\nWaiting!......")
+                    time.sleep(7)
+                elif stream_lnk_Path == "404 Not Here":
+                    time.sleep(8)# 21 ----
+                    print("\nPlease Check: C\\Program (or {x86}) \\Files\\Streamlink\\bin\\ Path")
+            else:
+                time.sleep(12)
+                
     else:
         break
-
-# choses directory of streamlink ----
-if isthere is True:
-    sLink_pth = r'C:\\Program Files\\Streamlink\\bin\\'
-elif isthere_86 is True:
-    sLink_pth = r'C:\\Program Files (x86)\\Streamlink\\bin\\'
 
 
 
@@ -77,7 +96,7 @@ sizes = ["worst", "160p", "360p", "480p", "720p", "720p60", "1080p", "1080p60","
 def check_size(Vid_size):
     # print(Vid_size in sizes) Checks if True/False
     if Vid_size in sizes:
-        return fr'cd {sLink_pth} && streamlink "{url_}" {Vid_size} --hls-segment-threads 5 -o "{file_path}"'
+        return fr'cd {stream_lnk_Path} && streamlink "{url_}" {Vid_size} --hls-segment-threads 5 -o "{file_path}"'
     else:
         return ("Enter a Valid Size")     
 Fake_ = ""
