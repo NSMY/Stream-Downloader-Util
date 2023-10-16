96e2f42f483a222aec60fbf4df05fda39d47b956
NSMY <oralsatin@gmail.com>
2023-10-16 14:37:10 +1000

    folded SDM.py into main instead of another file

--- Main.py
+++ Main.py
@@ -12,15 +12,22 @@ from datetime import datetime
 from queue import Queue
 from urllib.parse import urlparse
 
-import mux_vid as cpvs
 from pyperclip import copy, paste
 
 import auth_skip_ads_
 import funcs
 import init_files
+import mux_vid as cpvs
 
 
-def main_script():
+def main_script(download_with_Shutdown=None):
+
+    sd_type = None
+    if download_with_Shutdown:
+        sd_type = funcs.multi_choice_dialog('Manual shutdown Time or Shutdown after Completion', ['Auto', 'Manual'])
+        if sd_type == 'Manual':
+            funcs.manual_shutdown_timer()
+    
     # Retrieves Last item in Clipboard(ctrl v).
     url_ = paste().replace("?filter=archives&sort=time","")
     urlchk = funcs.is_url(url_)
@@ -137,8 +144,10 @@ def main_script():
     # Define a signal handler for SIGINT using a lambda function
     # changing ctrl+c to kill the subprocess instead of the terminal.
     handle_sigint = lambda signal, frame: funcs.kill_process(process)
+    
     # Register the signal handler for SIGINT
     signal.signal(signal.SIGINT, handle_sigint)
+    
     # Start a thread to wait for the subprocess to complete
     thread_popen = threading.Thread(target=funcs.wait_for_subprocess, args=(process,))
 
@@ -150,40 +159,44 @@ def main_script():
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 
-    print("\nCompletion Time:", datetime.now().strftime("---- %H:%M:%S ----\n"))
-    
-# def timer_shutdown(wait_time:int = 300):
-#     '''wait_time is the number of seconds'''
-#     os.system(f"shutdown -s -t {wait_time}") #FEATURE take out this sends shutdown cmd?
-#     with open('downloadCompleteTime.txt', 'w') as f:
-#         f.write(datetime.now().strftime("%H:%M:%S---------\n"))
-
-    # Plays Sound at this point.
-    winsound.PlaySound(sound='C:\\Windows\\Media\\Windows Proximity Notification.wav'
-                        , flags=winsound.SND_FILENAME)
-    
-    print("\nBecause of how Video is downloaded (Chunks) sometimes"
-                    " Re-Muxing is needed for smooth playback."
-                    " \n(Re-Mux)Make a Copy of this file?:\n ")
-    
-    # sends the file(name) to be mux'd via cpyVid_script____1.py
-    convert = funcs.multi_choice_dialog("Convert?", ["yes", "no"])
-    print(file_path)
-    if convert == "yes":
-        cpvs.mux(file_path)
-        print("\nDone!!")
+    if download_with_Shutdown:
+        if sd_type == 'Auto':
+            savepath = os.path.dirname(file_path)
+            with open(f'{savepath}/downloadCompleteTime.txt', 'w') as f:
+                f.write(datetime.now().strftime(f"{terminal_Naming}\nCompleted at:---- %H:%M:%S ----\n"))
+            cpvs.mux(file_path)
+            os.system("shutdown -s -t 200")
     else:
-        funcs.open_directory_Force_front(file_path)
-
-
-    print("\nRe Run Program? if Yes you need to copy the next URL"
-                " in the clipboard before answering this:\n")
-    exit = funcs.multi_choice_dialog("Run Again or Exit?", ["Run Again", "EXIT"])
-    if exit == "Run Again":
-        main_script()
-    else:
-        sys.exit ()
-    funcs.main_start()
+        print("\nCompletion Time:", datetime.now().strftime(
+            f"{terminal_Naming}\nCompleted at:---- %H:%M:%S ----\n")
+            )
+        
+        # Plays Sound at this point.
+        winsound.PlaySound(sound='C:\\Windows\\Media\\Windows Proximity Notification.wav'
+                            , flags=winsound.SND_FILENAME)
+        
+        print("\nBecause of how Video is downloaded (Chunks) sometimes"
+                        " Re-Muxing is needed for smooth playback."
+                        " \n(Re-Mux)Make a Copy of this file?:\n ")
+        
+        # sends the file(name) to be mux'd via mux_vid.py
+        convert = funcs.multi_choice_dialog("Convert?", ["yes", "no"])
+        print(file_path)
+        if convert == "yes":
+            cpvs.mux(file_path)
+            print("\nDone!!")
+        else:
+            funcs.open_directory_Force_front(file_path)
+        
+        
+        print("\nRe Run Program? if Yes you need to copy the next URL"
+                    " in the clipboard before answering this:\n"
+                    )
+        exit = funcs.multi_choice_dialog("Run Again or Exit?", ["Run Again", "EXIT"])
+        if exit == "Run Again":
+            funcs.main_start()
+        else:
+            sys.exit ()
 
 
 if __name__ == "__main__":
